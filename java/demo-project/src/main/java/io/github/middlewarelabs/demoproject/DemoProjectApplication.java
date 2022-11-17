1d4349258f082917a0bb0d06010771caf21f91a5
ACQ053 <keval@middleware.io>
2022-11-17 23:10:50 +0530

    interceptor example

--- java/demo-project/src/main/java/io/github/middlewarelabs/demoproject/DemoProjectApplication.java
+++ java/demo-project/src/main/java/io/github/middlewarelabs/demoproject/DemoProjectApplication.java
@@ -2,26 +2,21 @@ package io.github.middlewarelabs.demoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 // import io.github.middlewarelabs.agentapmjava.Logger;
 import io.github.middlewarelabs.agentapmjava.MwTracer;
 
+
 @SpringBootApplication
+@ComponentScan(basePackages = {"io.github.middlewarelabs.agentapmjava, io.github.middlewarelabs.demoproject"})
 public class DemoProjectApplication {
 
 	public static void main(String[] args) {
-		// Logger.info("info message");
-		// Logger.debug("debug message");
-		// Logger.warn("warn message");
-		// Logger.error("error message");
 		try {
-			MwTracer.track("myservice");
-		} catch (Throwable throwable) {
-			// parentSpan.setStatus(StatusCode.ERROR, "Something wrong with the parent span");
-		} finally {
-			/*closing the scope does not end the span, this has to be done manually*/
-			// parentSpan.end();
-		}
+			MwTracer.track("project1", "myservice");
+		} catch (Throwable throwable) {} 
+
+		
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
