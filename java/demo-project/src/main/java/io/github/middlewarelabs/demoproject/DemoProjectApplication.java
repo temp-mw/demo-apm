5ebec2b84cbd734c219d0474098f923d6ec1bd4d
ACQ053 <keval@middleware.io>
2022-11-18 10:39:48 +0530

    distributed tracing instructions added in JAVA APM

--- java/demo-project/src/main/java/io/github/middlewarelabs/demoproject/DemoProjectApplication.java
+++ java/demo-project/src/main/java/io/github/middlewarelabs/demoproject/DemoProjectApplication.java
@@ -2,21 +2,11 @@ package io.github.middlewarelabs.demoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-// import io.github.middlewarelabs.agentapmjava.Logger;
-import io.github.middlewarelabs.agentapmjava.MwTracer;
-
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"io.github.middlewarelabs.agentapmjava, io.github.middlewarelabs.demoproject"})
 public class DemoProjectApplication {
 
-	public static void main(String[] args) {
-		try {
-			MwTracer.track("project1", "myservice");
-		} catch (Throwable throwable) {} 
-
-		
+	public static void main(String[] args) {		
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
