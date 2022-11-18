28dd0a0c61fcb23eec31fd4bb581e0ad389488a2
ACQ053 <keval@middleware.io>
2022-11-18 10:44:49 +0530

    java example updated

--- java/demo-project/src/main/java/io/github/middlewarelabs/demoproject/DemoProjectApplication.java
+++ java/demo-project/src/main/java/io/github/middlewarelabs/demoproject/DemoProjectApplication.java
@@ -2,11 +2,16 @@ package io.github.middlewarelabs.demoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.middlewarelabs.agentapmjava.Logger;
 
 @SpringBootApplication
 public class DemoProjectApplication {
 
 	public static void main(String[] args) {		
+		Logger.info("info message");
+		Logger.debug("debug message");
+		Logger.warn("warn message");
+		Logger.error("error message");
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
