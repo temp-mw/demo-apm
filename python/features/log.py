4c36cb5bdb14c48c69f551cb9dbd3b27a95055f4
Meenal Mathur <meenalmathur23@gmail.com>
2023-02-02 13:05:24 +0530

    Update log.py

--- python/features/log.py
+++ python/features/log.py
@@ -1,5 +1,6 @@
 from apmpythonpackage import apmpythonclass
 tracker=apmpythonclass()
+tracker.mw_tracer({APM-PROJECT-NAME}, {APM-SERVICE-NAME})
 
 tracker.error('python error log sample')
 tracker.debug('python debug log sample')
