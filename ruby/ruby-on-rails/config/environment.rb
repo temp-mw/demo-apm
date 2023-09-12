9e62ea0dbd714abad123fbbe94e9d5211a49578b
Parth Dadhaniya <135583959+parth-dadhaniya@users.noreply.github.com>
2023-09-12 15:24:07 +0530

    Added the docker-compose.yaml and ruby-on-rails.yaml file for kubernetes example. (#64)

--- ruby/ruby-on-rails/config/environment.rb
+++ ruby/ruby-on-rails/config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+require 'middleware/ruby_gem'
+
+Middleware::RubyGem.init
+
 # Initialize the Rails application.
 Rails.application.initialize!
